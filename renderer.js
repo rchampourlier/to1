d2359b8be3726efdc18cacfcf421c4b5cc44b911
Romain Champourlier <public@rchampourlier.com>
2018-12-08 15:32:49 +0100

    Removed useless file

--- renderer.js
+++
@@ -1,8 +0,0 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
-
-import { fetchConfluenceInlineTasks } from "./fetch";
-
-console.log("test2");
-console.log(fetchConfluenceInlineTasks());
