124e370f08c593800fbc1735948e93da667568ae
Romain Champourlier <public@rchampourlier.com>
2018-12-08 15:29:54 +0100

    Rebooted with `electron-forge` and TS-React template
    Used [electron-forge](https://v6.electronforge.io) with the `react-typescript` template to rebuild the project. Could not get a local module file to be imported correctly with the previous setup. The project would compile but once loaded in Electron, the JS file could not be required correctly.
    
    This new setup is working fine, the `fetch` module was successfully imported from `app.tsx` and also enables live-reload.

--- HISTORY.md
+++ HISTORY.md
@@ -1,5 +1,11 @@
 # History
 
+## 2018-12-08
+
+Used [electron-forge](https://v6.electronforge.io) with the `react-typescript` template to rebuild the project. Could not get a local module file to be imported correctly with the previous setup. The project would compile but once loaded in Electron, the JS file could not be required correctly.
+
+This new setup is working fine, the `fetch` module was successfully imported from `app.tsx` and also enables live-reload.
+
 ## 2018-12-02
 
 - Started project
--- src/fetch.ts
+++ src/fetch.ts
@@ -5,7 +5,7 @@
 // - Jira mentions
 // - Trello notifications
 // - Google Docs mentions
-const request = require('request');
+import * as request from 'request';
 
 interface Todo {
   origin: string;
@@ -17,31 +17,31 @@ interface Todo {
   due: string;
 }
 
-var ATLASSIAN_USERNAME = process.env.ATLASSIAN_USERNAME;
-var ATLASSIAN_PASSWORD = process.env.ATLASSIAN_PASSWORD;
-var TRELLO_KEY = process.env.TRELLO_KEY;
-var TRELLO_TOKEN = process.env.TRELLO_TOKEN;
+const ATLASSIAN_USERNAME = process.env.ATLASSIAN_USERNAME;
+const ATLASSIAN_PASSWORD = process.env.ATLASSIAN_PASSWORD;
+const TRELLO_KEY = process.env.TRELLO_KEY;
+const TRELLO_TOKEN = process.env.TRELLO_TOKEN;
 
 // Confluence inline-tasks
-var todos: Array<Todo> = [];
+let todos: Array<Todo> = [];
 
-var fetchConfluenceInlineTasks = () => {
+let fetchConfluenceInlineTasks = () => {
   request('https://jobteaser.atlassian.net/wiki/rest/mywork/1/task', {
     'auth': {
       'user': ATLASSIAN_USERNAME,
       'pass': ATLASSIAN_PASSWORD,
     }}, (error, response, body) => {
       if (error == null && response && response.statusCode === 200) {
-        var tasks = JSON.parse(body);
-        tasks.forEach( (task, index) => {
+        let tasks = JSON.parse(body);
+        tasks.forEach( (task: any) => {
           todos.push({
             origin: 'Confluence',
             title: task.title,
             sourceTitle: task.item.title,
             sourceUrl: task.item.url,
-            details: null,
+            details: '',
             created: task.created,
-            due: null,
+            due: ''
           });
         });
         console.log('todos:', todos);
@@ -55,28 +55,28 @@ var fetchConfluenceInlineTasks = () => {
   );
 };
 
-var fetchTrelloNotifications = () => {
+let fetchTrelloNotifications = () => {
   request('https://api.trello.com/1/members/me/notifications', {
     'qs': {
       'key': TRELLO_KEY,
       'token': TRELLO_TOKEN,
     }}, (error, response, body) => {
       if (error == null && response && response.statusCode === 200) {
-        var notifications = JSON.parse(body);
-        notifications.forEach( (notification, index) => {
+        let notifications = JSON.parse(body);
+        notifications.forEach( (notification: any) => {
           if (notification.unread) {
-            var data = notification.data;
-            var board = data.board, list = data.list, card = data.card;
             switch (notification.type) {
               case 'makeAdminOfOrganization':
+                let data = notification.data;
+                let board = data.board, list = data.list, card = data.card;
                 todos.push({
                   origin: 'Trello',
                   title: 'Made admin of organization `' + data.organization.name,
-                  sourceTitle: null,
-                  sourceUrl: null,
-                  details: null,
+                  sourceTitle: '',
+                  sourceUrl: '',
+                  details: '',
                   created: notification.date,
-                  due: null,
+                  due: '',
                 });
                 break;
               case 'cardDueSoon':
@@ -85,55 +85,50 @@ var fetchTrelloNotifications = () => {
                   title: 'Card due soon',
                   sourceTitle: board.name + ' > ' + card.name,
                   sourceUrl: 'https://trello.com/c/' + card.shortLink,
-                  details: null,
+                  details: '',
                   created: notification.date,
                   due: card.due,
                 });
                 break;
               case 'commentCard':
-                var cardFullName = board.name + ' > ' + card.name;
                 todos.push({
                   origin: 'Trello',
-                  title: 'Card `' + cardFullName + '` commented',
+                  title: 'Card `' + board.name + ' > ' + card.name + '` commented',
                   sourceTitle: board.name + ' > ' + card.name,
                   sourceUrl: 'https://trello.com/c/' + card.shortLink,
                   details: notification.data.text,
                   created: notification.date,
-                  due: null,
+                  due: '',
                 });
                 break;
               case 'createdCard':
-                var cardFullName = board.name + ' > ' + list.name + ' > ' + card.name;
                 todos.push({
                   origin: 'Trello',
-                  title: 'Card `' + cardFullName + '` created by ' + notification.memberCreator.fullName,
+                  title: 'Card `' + board.name + ' > ' + card.name + '` created by ' + notification.memberCreator.fullName,
                   sourceTitle: board.name + ' > ' + card.name,
                   sourceUrl: 'https://trello.com/c/' + card.shortLink,
                   details: notification.data.text,
                   created: notification.date,
-                  due: null,
+                  due: '',
                 });
                 break;
               case 'changeCard':
-                var title: string;
+                let title: string;
+                let cardFullName = board.name + ' > ' + list.name + ' > ' + card.name;
                 if (data.listBefore) {
                   title = 'Card `' + cardFullName + '` moved from list `' + notification.data.listBefore + '`';
-                }
-                else if (data.old && data.old.due !== undefined) { // data.old.due may be null
+                } else if (data.old && data.old.due !== undefined) { // data.old.due may be null
                   title = 'Card `' + cardFullName + '` changed due to ' + data.card.due + ' (was ' + data.old.due + ')';
-                }
-                else if (data.old && data.old.closed !== undefined) {
+                } else if (data.old && data.old.closed !== undefined) {
                   if (data.old.closed === true) {
                     title = 'Card `' + cardFullName + '` archived';
                   } else {
                     title = 'Card `' + cardFullName + '` unarchived';
                   }
-                }
-                else {
+                } else {
                   title = 'N/A';
                   console.log(notification);
                 }
-                var data = notification.data;
                 todos.push({
                   origin: 'Trello',
                   title: title,
@@ -141,11 +136,11 @@ var fetchTrelloNotifications = () => {
                   sourceUrl: 'https://trello.com/c/' + card.shortLink,
                   details: notification.data.text,
                   created: notification.date,
-                  due: null,
+                  due: '',
                 });
                 break;
               case 'mentionedOnCard':
-                var cardFullName = board.name + ' > ' + card.name;
+                cardFullName = board.name + ' > ' + card.name;
                 todos.push({
                   origin: 'Trello',
                   title: 'Mentioned on card `' + cardFullName + '`',
@@ -153,11 +148,11 @@ var fetchTrelloNotifications = () => {
                   sourceUrl: 'https://trello.com/c/' + card.shortLink,
                   details: data.text,
                   created: notification.date,
-                  due: null,
+                  due: '',
                 });
                 break;
               case 'addAttachmentToCard':
-                var cardFullName = board.name + ' > ' + card.name;
+                cardFullName = board.name + ' > ' + card.name;
                 todos.push({
                   origin: 'Trello',
                   title: 'Added attachment `' + data.name + '` to card `' + cardFullName + '`',
@@ -165,7 +160,7 @@ var fetchTrelloNotifications = () => {
                   sourceUrl: 'https://trello.com/c/' + card.shortLink,
                   details: data.text,
                   created: notification.date,
-                  due: null,
+                  due: '',
                 });
                 break;
               default:
@@ -185,7 +180,7 @@ var fetchTrelloNotifications = () => {
   );
 };
 
-export { 
+export {
   fetchConfluenceInlineTasks,
   fetchTrelloNotifications
 };
